072f9e9ab413ce9d74fa3bfadfb8d8100f3dd7d1
Stepan <soulsteff@gmail.com>
2024-03-11 14:22:10 +0300

    ver5.1

--- Test/HW3/MainHWTest.java
+++ Test/HW3/MainHWTest.java
@@ -2,7 +2,6 @@ package HW3;
 
 import Main.hw3.MainHW;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
